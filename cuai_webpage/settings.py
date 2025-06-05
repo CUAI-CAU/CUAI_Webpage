60e4de561a4351950fb3e25baac371e260470423
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2025-06-05 15:17:33 +0900

    cors fix

--- cuai_webpage/settings.py
+++ cuai_webpage/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -137,3 +138,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = []
+#     "http://localhost:3000", # 프론트엔드 개발 서버 주소
+#     "http://127.0.0.1:3000", # 프론트엔드 개발 서버 주소
+
+CORS_ALLOWED_ORIGINS.append(os.getenv("FRONTEND_TEMP_DOMAIN"))
