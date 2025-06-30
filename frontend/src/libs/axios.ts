00437d1f3c4ced49ec8fefe8d9c15ffb7d2502d0
Sowon Kim <sso626262@naver.com>
2025-06-30 14:32:03 +0900

    frontend/fix: 도메인 주소 env 변수명 수정

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.SERVER_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     // baseURL: '/server',
     // withCredentials: true,
 })
