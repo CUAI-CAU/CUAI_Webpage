df81e3c41a8379f12aa1fcb770d928b447abae48
Sowon Kim <sso626262@naver.com>
2025-07-05 22:34:06 +0900

    frontend/build: next.config.ts에서 rewrites 삭제

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,14 +4,14 @@ const nextConfig: NextConfig = {
     images: {
         domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
     },
-    async rewrites() {
-        return [
-            {
-                source: '/server/:path*',
-                destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-            },
-        ]
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/server/:path*',
+    //             destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+    //         },
+    //     ]
+    // },
 }
 
 export default nextConfig
